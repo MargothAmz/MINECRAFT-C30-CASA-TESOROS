d2e2b28ebb41c06a8b6fd62364a7259b47a4cf73
MargothAmz <93557612+MargothAmz@users.noreply.github.com>
2022-06-12 12:59:16 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var ball,lowerWall,upperWall,leftWall;
+
+
+
+
+
+
+
+function setup(){
+  createCanvas(950, 470);
+  
+  
+  
+
+
+}
+
+
+function draw(){
+  background(220);
+    
+   
+    
+  drawSprites();
+  
+}
+
+
+
